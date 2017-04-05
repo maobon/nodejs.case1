fc36a00d3d9eeec71db4aa7c87d5b0637c561ddf
maobon <maobonhaha@gmail.com>
2017-04-05 20:39:02 +0800

    add页面添加英雄, 页面提交form表单, 服务端接受并写入db文件

--- app.js
+++ app.js
@@ -9,12 +9,19 @@ var template = require('art-template');
 var server = http.createServer();
 
 server.on('request', function (req, res) {
+    // 请求路径
     var url = req.url;
+    // 请求方法
     var method = req.method.toLowerCase();
 
+    // 初始化 render
     initRender(res);
 
+    /**
+     * index 首页
+     */
     if (method === 'get' && url === '/') {
+        // 开放首页
         fs.readFile('./db.json', 'utf8', function (err, data) {
             if (err) throw err;
 
@@ -22,8 +29,91 @@ server.on('request', function (req, res) {
             res.render('index', data);
         })
 
-    } else if (method === 'get' && url.indexOf('/node_modules/') === 0
+    }
+
+    /**
+     * add 添加英雄
+     */
+    else if (method === 'get' && url === '/add') {
+        res.render('add', {
+            title: 'NodeJS'
+        });
+
+    } else if (method === 'post' && url === '/add') {
+
+        // 接收数据
+        // (查询字符串格式) 客户端post发送过来的数据
+        var content = ''; // username=xinyi&age=28&home=beijing
+
+        // request对象的data事件中接收
+        // 每接收一段 就回调一次该事件
+        // form表单可能一段一段发送过来
+        req.on('data', function (chunk) {
+            content += chunk;
+        });
+
+        // request对象的end事件 参数传递完成时回调
+        req.on('end', function () {
+            content = decodeURI(content);
+
+            // 将查询字符串格式的数据转为{ }
+            var body = {};
+            content.split('&').forEach(function (item) {
+                var temp = item.split('=');
+                body[temp[0]] = temp[1];
+            });
+
+            // 先查询数据库文件 修改后再写回去
+            fs.readFile('./db.json', function (err, data) {
+                // 读文件 data是string类型
+
+                if (err) {
+                    res.end(JSON.stringify({
+                        err_code: 500,
+                        message: err.message
+                    }))
+                }
+
+                // String转{ }
+                data = JSON.parse(data);
+                data.heros.push({
+                    id: 1,
+                    name: body.name,
+                    gender: body.gender,
+                    avatar: 'img/133.jpg'
+                });
+
+                // { }转String 准备写入数据库
+                data = JSON.stringify(data, null, '    ');
+
+                // 写入数据库
+                fs.writeFile('./db.json', data, function (err) {
+                    if (err) {
+                        // 发生错误 返回错误信息给服务端 JSON格式
+                        res.end(JSON.stringify({
+                            err_code: 500,
+                            message: err.message
+                        }))
+                    }
+
+                    // 写入成功 返回给客户端
+                    res.end(JSON.stringify({
+                        err_code: 0,
+                    }));
+                })
+
+
+            })
+
+        });
+    }
+
+    /**
+     * 开放资源的请求
+     */
+    else if (method === 'get' && url.indexOf('/node_modules/') === 0
         || url.indexOf('/img/') === 0) {
+        // 开放页面资源请求
 
         var filePath = '.' + url;
         fs.readFile(filePath, function (err, data) {
